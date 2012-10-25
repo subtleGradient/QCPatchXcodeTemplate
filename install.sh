fede7721d37264f4f891c3b6cb258ce86912b4db
Thomas Aylott <aylott@fb.com>
2012-10-24 19:52:57 -0700

    Fixes #3 Add (optional) support for ARC
    After creating a new SkankySDK project, use the **Refactor > Convert to Objective-C ARC…** command to convert your project to ARC.

--- install.sh
+++ install.sh
@@ -9,3 +9,9 @@ cp -R "sdk/Project Templates/Kineme SkankySDK" "/Applications/Xcode.app/Contents
 
 mkdir -p /Library/Frameworks/SkankySDK.framework/Headers
 cp include/* /Library/Frameworks/SkankySDK.framework/Headers/
+
+mkdir -p "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/System/Library/Frameworks/SkankySDK.framework/Headers/"
+cp include/* "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/System/Library/Frameworks/SkankySDK.framework/Headers/"
+
+mkdir -p "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks/SkankySDK.framework/Headers/"
+cp include/* "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks/SkankySDK.framework/Headers/"
